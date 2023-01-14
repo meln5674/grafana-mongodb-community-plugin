050adcf7547edec44ab4a952784230400881107b
Andrew Melnick <meln5674@kettering.edu>
2023-01-14 12:52:39 -0700

    Refactor merged changes

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -6,13 +6,13 @@ import {
   Input,
   SecretInput,
   TextArea,
+  SecretTextArea,
   Field,
   Switch,
-  LegacyForms,
 } from '@grafana/ui';
-const { FormField } = LegacyForms;
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { MongoDBDataSourceOptions, MongoDBSecureJsonData } from './types';
+
 
 interface Props extends DataSourcePluginOptionsEditorProps<MongoDBDataSourceOptions> {}
 
@@ -24,7 +24,7 @@ export class ConfigEditor extends PureComponent<Props, State> {
     const jsonData = {
       ...options.jsonData,
       url: event.target.value,
-    };
+    } as MongoDBDataSourceOptions;
     onOptionsChange({ ...options, jsonData });
   };
   onTLSCAChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -108,13 +108,147 @@ export class ConfigEditor extends PureComponent<Props, State> {
     });
   };
 
-  readonly shortWidth = 15;
-  readonly longWidth = 60;
+  readonly shortWidth = 24;
+  readonly longWidth = 56;
+  readonly beginCert = "-----BEGIN CERTIFICATE-----";
+  readonly endCert = "-----END CERTIFICATE-----";
+  readonly beginKey = "-----BEGIN RSA PRIVATE KEY-----";
+  readonly endKey = "-----END RSA PRIVATE KEY-----";
 
-  render() {
+  renderCredentials() {
+    const { options } = this.props;
+    const { secureJsonFields } = options;
+    const secureJsonData = (options.secureJsonData || {}) as MongoDBSecureJsonData;
+
+    return (
+      <>
+        <InlineFieldRow>
+          <InlineField labelWidth={this.shortWidth} label="Username">
+            <SecretInput
+              width={this.longWidth}
+              isConfigured={(secureJsonFields && secureJsonFields.username) as boolean}
+              value={secureJsonData.username || ''}
+              placeholder="Username"
+              onReset={this.onResetCredential}
+              onChange={this.onUsernameChange}
+            ></SecretInput>
+          </InlineField>
+          <InlineField label="Password" labelWidth={this.shortWidth}>
+            <SecretInput
+              width={this.longWidth}
+              isConfigured={(secureJsonFields && secureJsonFields.password) as boolean}
+              value={secureJsonData.password || ''}
+              label="Password"
+              placeholder="Password"
+              onReset={this.onResetCredential}
+              onChange={this.onPasswordChange}
+            ></SecretInput>
+          </InlineField>
+        </InlineFieldRow>
+      </>
+    )
+  }
+
+  renderTls() {
+    const { options } = this.props;
+    const { jsonData } = options;
+
+    return (
+      <>
+        <Field label="TLS Enabled">
+          <Switch
+            value={jsonData.tls || false}
+            onChange={this.onTLSChange}
+          />
+        </Field>
+        { jsonData.tls ? this.renderTlsFields() : null }
+      </>
+    )
+  }
+
+  renderTlsFields() {
+    const { options } = this.props;
+    const { jsonData } = options;
+
+    return (
+      <>
+        <Field label="Insecure (Skip Verification)">
+          <Switch
+            value={jsonData.tlsInsecure || false}
+            onChange={this.onTLSInsecureChange}
+          />
+        </Field>
+        { jsonData.tlsInsecure ? null : this.renderTlsVerification() }
+        { this.renderTlsClient() }
+      </>
+    )
+  }
+
+  renderTlsVerification() {
+    const { options } = this.props;
+    const { jsonData } = options;
+    
+    return (
+      <> 
+        <Field label="TLS Certificate Authority">
+          <TextArea
+            value={jsonData.tlsCa || ''}
+            placeholder={`${this.beginCert}\n...\n${this.endCert}`}
+            onChange={this.onTLSCAChange}
+            cols={this.longWidth}
+          />
+        </Field>
+        <InlineField
+            labelWidth={this.shortWidth}
+            label="Expected Server Name"
+            tooltip="If your server's certificates are for a different hostname than you use to connect, specify that different hostname here"
+            >
+          <Input
+            width={this.longWidth}
+            name="tlsServerName"
+            type="text"
+            onChange={this.onTLSServerNameChange}
+            value={jsonData.tlsServerName || ''}
+            placeholder="some.other.hostname"
+          ></Input>
+        </InlineField>
+      </>
+    )
+  }
+
+  renderTlsClient() {
     const { options } = this.props;
     const { jsonData, secureJsonFields } = options;
     const secureJsonData = (options.secureJsonData || {}) as MongoDBSecureJsonData;
+
+    return (
+      <>
+        <Field label="TLS Certificate">
+          <TextArea
+            value={jsonData.tlsCertificate || ''}
+            placeholder={`${this.beginCert}\n...\n${this.endCert}`}
+            onChange={this.onTLSCertificateChange}
+            cols={this.longWidth}
+          />
+        </Field>
+        <br/>
+        <Field label="TLS Certificate Key">
+          <SecretTextArea
+            value={secureJsonData.tlsCertificateKey || ''}
+            isConfigured={(secureJsonFields && secureJsonFields.tlsCertificateKey) as boolean}
+            placeholder={`${this.beginKey}\n...\n${this.endKey}`}
+            onChange={this.onTLSCertificateKeyChange}
+            onReset={this.onResetCredential}
+            cols={this.longWidth}
+          />
+        </Field>
+      </>
+    )
+  }
+
+  render() {
+    const { options } = this.props;
+    const { jsonData } = options;
 
     return (
       <>
@@ -129,88 +263,9 @@ export class ConfigEditor extends PureComponent<Props, State> {
               placeholder="mongodb[+svc]://hostname:port[,hostname:port][/?key=value]"
             ></Input>
           </InlineField>
-          <InlineFieldRow>
-            <InlineField labelWidth={this.shortWidth} label="Username">
-              <SecretInput
-                width={this.shortWidth}
-                isConfigured={(secureJsonFields && secureJsonFields.username) as boolean}
-                value={secureJsonData.username || ''}
-                placeholder="Username"
-                onReset={this.onResetCredential}
-                onChange={this.onUsernameChange}
-              ></SecretInput>
-            </InlineField>
-            <InlineField label="Password" labelWidth={this.shortWidth}>
-              <SecretInput
-                width={this.shortWidth}
-                isConfigured={(secureJsonFields && secureJsonFields.password) as boolean}
-                value={secureJsonData.password || ''}
-                label="Password"
-                placeholder="Password"
-                onReset={this.onResetCredential}
-                onChange={this.onPasswordChange}
-              ></SecretInput>
-            </InlineField>
-          </InlineFieldRow>
-        </FieldSet>
-        <Field label="TLS Enabled">
-            <Switch
-              value={jsonData.tls || false}
-              onChange={this.onTLSChange}
-            />
-        </Field>
-        { jsonData.tls ?
-          <div>
-            <Field label="Insecure (Skip Verification)">
-                <Switch
-                  value={jsonData.tlsInsecure || false}
-                  onChange={this.onTLSInsecureChange}
-                />
-            </Field>
-            { !jsonData.tlsInsecure ?
-              <> 
-              <Field label="TLS Certificate Authority">
-                <TextArea
-                  value={jsonData.tlsCa || ''}
-                  placeholder={"-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----"}
-                  onChange={this.onTLSCAChange}
-                  cols={100}
-                />
-              </Field>
-              <FormField
-                label="Expected Server Name"
-                labelWidth={16}
-                inputWidth={40}
-                onChange={this.onTLSServerNameChange}
-                value={jsonData.tlsServerName || ''}
-                placeholder="some.other.hostname"
-                tooltip="If your server's certificates are for a different hostname than you use to connect, specify that different hostname here"
-              />
-              <br/>
-              </>
-              : null
-            }
-              <Field label="TLS Certificate">
-                <TextArea
-                  value={jsonData.tlsCertificate || ''}
-                  placeholder={"-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----"}
-                  onChange={this.onTLSCertificateChange}
-                  cols={100}
-                />
-              </Field>
-              <br/>
-              <Field label="TLS Certificate Key">
-                <TextArea
-                  value={secureJsonData.tlsCertificateKey || ''}
-                  placeholder={secureJsonFields.tlsCertificateKey ? "-----BEGIN RSA PRIVATE KEY-----\n(This field has been set and is hidden)\n-----END RSA PRIVATE KEY-----" : "-----BEGIN RSA PRIVATE KEY-----\n(This field has not been set)\n-----END RSA PRIVATE KEY-----"}
-                  onChange={this.onTLSCertificateKeyChange}
-                  cols={100}
-                />
-              </Field>
-
-          </div>
-        : null
-      }
+          { this.renderCredentials() }
+          { this.renderTls() }
+        </FieldSet>            
       </>
     );
   }
